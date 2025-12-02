7374c949f106cef9439bcfaa1f66ac1d65b90bdf
ArthurG1t <arthurteixeira.git@gmail.com>
2025-12-02 18:06:41 -0300

    merge

---
+++ backend/src/routes/userRoutes.ts
@@ -0,0 +1,31 @@
+import { Router } from "express";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
+import { getUserProfile } from "../controllers/userController.js";
+
+const router = Router();
+
+// Rota protegida para obter dados do usuário autenticado
+router.get("/me", authMiddleware, getUserProfile);
+
+export default router;
+/**
+ * @swagger
+ * tags:
+ *   name: Usuários
+ *   description: Endpoints relacionados aos usuários
+ */
+
+/**
+ * @swagger
+ * /users/me:
+ *   get:
+ *     summary: Obter dados do usuário autenticado
+ *     tags: [Usuários]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Dados do usuário retornados com sucesso
+ *       401:
+ *         description: Token ausente ou inválido
+ */
